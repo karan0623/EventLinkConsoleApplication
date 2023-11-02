859d85d41932e67d161b5d161bc0b86168d99ed7
Karan Patel <patel3k7@mail.uc.edu>
2023-11-02 14:39:36 -0400

    Added InstagramPostsController, InstagramPosts class, and InstagramPostsContext class

---
+++ EventLinkConsoleApplication/Controllers/InstagramPostsController.cs
@@ -0,0 +1,42 @@
+﻿using EventLink.Models;
+using Newtonsoft.Json;
+
+namespace EventLink.Controllers
+{
+    public class InstagramPostsController
+    {
+        private InstagramPostsContext context { get; set; }
+        public InstagramPostsController(InstagramPostsContext ctx)
+        {
+            context = ctx;
+
+        }
+
+        public async Task<InstagramPosts> GetJsonDataFromApiAsync()
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage response = await client.GetAsync("https://api.apify.com/v2/actor-tasks/exclusive_commode~eventlink---instagram-post-scraper/run-sync-get-dataset-items?token=apify_api_Uea6k2FqNtwergHaVTQ5YiVQm2Q4d80BNqpH");
+                if (response.IsSuccessStatusCode)
+                {
+                    string json = await response.Content.ReadAsStringAsync();
+                    InstagramPosts data = JsonConvert.DeserializeObject<InstagramPosts>(json);
+                    return data;
+                }
+                else
+                {
+                    // Handle the error response
+                    throw new HttpRequestException($"API request failed with status code {response.StatusCode}");
+                }
+            }
+        }
+        public async Task InsertDataIntoDatabaseAsync(List<InstagramPosts> data)
+        {
+            using (var context = new InstagramPostsContext(options: null))
+            {
+                context.InstagramPosts.AddRange(data);
+                await context.SaveChangesAsync();
+            }
+        }
+    }
+}
---
+++ EventLinkConsoleApplication/Models/InstagramPostsContext.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EventLink.Models
+{
+
+    public class InstagramPostsContext: DbContext
+    {
+        public InstagramPostsContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<InstagramPosts> InstagramPosts { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            modelBuilder.Entity<InstagramPosts>().Property(e => e.DisplayUrl).IsRequired();
+
+            modelBuilder.Entity<InstagramPosts>().Property(e => e.Caption).IsRequired();
+
+            modelBuilder.Entity<InstagramPosts>().Property(e => e.Hashtags).IsRequired();
+
+            modelBuilder.Entity<InstagramPosts>().Property(e => e.LocationName).IsRequired();
+
+            modelBuilder.Entity<InstagramPosts>().Property(e => e.Timestamp).IsRequired();
+
+            modelBuilder.Entity<InstagramPosts>().Property(e => e.Images).IsRequired();
+
+            modelBuilder.Entity<InstagramPosts>().Property(e => e.VideoUrl).IsRequired();
+
+            modelBuilder.Entity<InstagramPosts>().Property(e => e.OwnerUsername).IsRequired();
+
+            modelBuilder.Entity<InstagramPosts>().Property(e => e.OwnerFullName).IsRequired();
+
+
+
+        }
+    }
+}

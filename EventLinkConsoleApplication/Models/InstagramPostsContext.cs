cd9ef63ca134e437de69901b277d0e4cd82ae861
Karan Patel <patel3k7@mail.uc.edu>
2023-11-08 12:36:27 -0500

    updated data that needs to go to the table

--- EventLinkConsoleApplication/Models/InstagramPostsContext.cs
+++ EventLinkConsoleApplication/Models/InstagramPostsContext.cs
@@ -14,6 +14,7 @@ namespace EventLink.Models
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            modelBuilder.Entity<InstagramPosts>().Property(e => e.Id).IsRequired();
 
             modelBuilder.Entity<InstagramPosts>().Property(e => e.DisplayUrl).IsRequired();
 
